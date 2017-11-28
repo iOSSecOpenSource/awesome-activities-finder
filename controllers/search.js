29eb98cc828eb8bec2660b442e039cb4ca79ffca
Jeffchiucp <jeffchiu2000@gmail.com>
2017-11-28 15:42:51 -0800

    added promise.all() for search issue #4,#5,#6,#7

--- controllers/search.js
+++ controllers/search.js
@@ -6,6 +6,28 @@ const yelpKey = process.env.yelpKey;
 
 module.exports = function (app) {
 
+  // const yelpPromise = yelp.accessToken(yelpId, yelpKey)
+  // const rpPromise = rp(options)
+  // const eventBeightPromise = eventbriteAPI({})
+
+  /*
+
+  Promise.all([yelpPromise, rpPromise, eventBeightPromise]).then((values) = > {
+    // handle yelp results
+    const yelpResults = values[0]
+    handleYelp(yelpResults)
+
+    // handle rp results
+    const yelpResults = values[1]
+
+    // handle eventbright results
+    const yelpResults = values[2]
+
+  })
+
+  */
+
+
     app.get('/search', function(req, res) {
         res.render('search', {})
     });
@@ -16,21 +38,21 @@ module.exports = function (app) {
           location: req.body.place
       };
 
+
       yelp.accessToken(yelpId, yelpKey).then(response => {
           const client = yelp.client(response.jsonBody.access_token);
-
-          client.search(searchRequest).then(response => {
-              const firstResult = response.jsonBody.businesses[0];
-              const prettyJson = JSON.stringify(firstResult, null, 4);
-              console.log(response.jsonBody.businesses);
-              return response.jsonBody.businesses
-          }).then(result => {
-              console.log(result.id)
-              res.render('results', { yelpDatas: result })
-          }).catch(err => {
-              console.log(err)
-          });
-      });
+          return client.search(searchRequest)
+      }).then(response => {
+          const firstResult = response.jsonBody.businesses[0];
+          const prettyJson = JSON.stringify(firstResult, null, 4);
+          console.log(response.jsonBody.businesses);
+          return response.jsonBody.businesses
+      }).then(result => {
+          console.log(result.id)
+          res.render('results', { yelpDatas: result })
+      }).catch(err => {
+          console.log(err)
+      });;
 
         // res.redirect('/')
     });
